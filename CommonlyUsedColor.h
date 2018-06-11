e144b5d2776b0d7ec842334348c202de6aa49353
zyisme@gmail.com <zyisme@gmail.com>
2018-06-11 15:47:37 +0800

    初始提交

---
+++ CommonlyUsedColor.h
@@ -0,0 +1,284 @@
+//
+//  CommonlyUsedColor.h
+//  Color
+//
+//  Created by 雨张 on 2018/6/11.
+//  Copyright © 2018年 雨张. All rights reserved.
+//
+
+#ifndef CommonlyUsedColor_h
+#define CommonlyUsedColor_h
+
+// 浅粉色
+#define LIGHT_PINK        [UIColor colorWithRed:255/255.0 green:182/255.0 blue:193/255.0 alpha:1.0]
+// 粉红
+#define PINK              [UIColor colorWithRed:255/255.0 green:192/255.0 blue:203/255.0 alpha:1.0]
+// 猩红
+#define CRIMSON           [UIColor colorWithRed:220/255.0 green:20/255.0 blue:60/255.0 alpha:1.0]
+// 脸红的淡紫色
+#define LAVENDER_BLUSH    [UIColor colorWithRed:255/255.0 green:240/255.0 blue:245/255.0 alpha:1.0]
+// 苍白的紫罗兰红色
+#define PALEVIOLET_RED    [UIColor colorWithRed:219/255.0 green:112/255.0 blue:147/255.0 alpha:1.0]
+// 热情的粉红
+#define HOT_PINK          [UIColor colorWithRed:255/255.0 green:105/255.0 blue:180/255.0 alpha:1.0]
+// 深粉色
+#define DEEP_PINK         [UIColor colorWithRed:225/255.0 green:20/255.0 blue:147/255.0 alpha:1.0]
+// 适中的紫罗兰红色
+#define MEDIUM_VIOLET_RED [UIColor colorWithRed:119/255.0 green:21/255.0 blue:133/255.0 alpha:1.0]
+// 兰花的紫色
+#define ORCHID            [UIColor colorWithRed:218/255.0 green:112/255.0 blue:214/255.0 alpha:1.0]
+// 蓟
+#define THISTLE           [UIColor colorWithRed:216/255.0 green:119/255.0 blue:216/255.0 alpha:1.0]
+// 李子
+#define PLUM              [UIColor colorWithRed:221/255.0 green:160/255.0 blue:221/255.0 alpha:1.0]
+// 紫罗兰
+#define VIOLET            [UIColor colorWithRed:238/255.0 green:130/255.0 blue:238/255.0 alpha:1.0]
+// 洋红
+#define MAGENTA           [UIColor colorWithRed:255/255.0 green:0/255.0 blue:255/255.0 alpha:1.0]
+// 灯笼海棠(紫红色)
+#define FUCHSIA           [UIColor colorWithRed:255/255.0 green:0/255.0 blue:255/255.0 alpha:1.0]
+// 深洋红色
+#define DARK_MAGENTA      [UIColor colorWithRed:139/255.0 green:0/255.0 blue:139/255.0 alpha:1.0]
+// 紫色
+#define PURPLE            [UIColor colorWithRed:128/255.0 green:0/255.0 blue:128/255.0 alpha:1.0]
+// 适中的兰花紫
+#define MEDIUM_ORCHID     [UIColor colorWithRed:186/255.0 green:85/255.0 blue:211/255.0 alpha:1.0]
+// 深紫罗兰色
+#define DARK_VOILET       [UIColor colorWithRed:148/255.0 green:0/255.0 blue:211/255.0 alpha:1.0]
+// 深兰花紫
+#define DARK_ORCHID       [UIColor colorWithRed:153/255.0 green:50/255.0 blue:204/255.0 alpha:1.0]
+// 靛青
+#define INDIGO            [UIColor colorWithRed:75/255.0 green:0/255.0 blue:130/255.0 alpha:1.0]
+// 深紫罗兰的蓝色
+#define BLUE_VIOLET       [UIColor colorWithRed:138/255.0 green:43/255.0 blue:226/255.0 alpha:1.0]
+// 适中的紫色
+#define MEDIUM_PURPLE     [UIColor colorWithRed:147/255.0 green:112/255.0 blue:219/255.0 alpha:1.0]
+// 适中的板岩暗蓝灰色
+#define MEDIUM_SLATE_BLUE [UIColor colorWithRed:123/255.0 green:104/255.0 blue:238/255.0 alpha:1.0]
+// 板岩暗蓝灰色
+#define SLATE_BLUE        [UIColor colorWithRed:106/255.0 green:90/255.0 blue:205/255.0 alpha:1.0]
+// 深岩暗蓝灰色
+#define DARK_SLATE_BLUE   [UIColor colorWithRed:72/255.0 green:61/255.0 blue:139/255.0 alpha:1.0]
+// 薰衣草花的淡紫色
+#define LAVENDER          [UIColor colorWithRed:230/255.0 green:230/255.0 blue:255/255.0 alpha:1.0]
+// 幽灵的白色
+#define GHOSTWHITE        [UIColor colorWithRed:248/255.0 green:248/255.0 blue:255/255.0 alpha:1.0]
+// 纯蓝
+#define BLUE              [UIColor colorWithRed:0/255.0 green:0/255.0 blue:255/255.0 alpha:1.0];
+// 适中的蓝色
+#define MEDIUM_BLUE       [UIColor colorWithRed:0/255.0 green:0/255.0 blue:205/255.0 alpha:1.0];
+// 午夜的蓝色
+#define MIDNIGHT_BLUE     [UIColor colorWithRed:25/255.0 green:25/255.0 blue:112/255.0 alpha:1.0];
+// 深蓝色
+#define DARK_BLUE         [UIColor colorWithRed:0/255.0 green:0/255.0 blue:239/255.0 alpha:1.0];
+// 海军蓝
+#define NAVY              [UIColor colorWithRed:0/255.0 green:0/255.0 blue:128/255.0 alpha:1.0];
+// 宝蓝
+#define ROYALBLUE         [UIColor colorWithRed:65/255.0 green:105/255.0 blue:225/255.0 alpha:1.0];
+// 矢车菊的蓝色
+#define CORNFLOWER_BLUE   [UIColor colorWithRed:100/255.0 green:149/255.0 blue:237/255.0 alpha:1.0]
+// 淡钢蓝
+#define LIGHT_STEEL_BLUE  [UIColor colorWithRed:176/255.0 green:196/255.0 blue:222/255.0 alpha:1.0]
+// 浅石板灰
+#define LIGHT_SLATE_GRAY  [UIColor colorWithRed:119/255.0 green:136/255.0 blue:153/255.0 alpha:1.0]
+// 石板灰
+#define SLATE_GRAY        [UIColor colorWithRed:112/255.0 green:128/255.0 blue:144/255.0 alpha:1.0]
+// 道奇蓝
+#define DODER_BLUE        [UIColor colorWithRed:30/255.0 green:144/255.0 blue:255/255.0 alpha:1.0]
+// 爱丽丝蓝
+#define ALICE_BLUE        [UIColor colorWithRed:240/255.0 green:248/255.0 blue:255/255.0 alpha:1.0]
+// 钢蓝
+#define STEEL_BLUE        [UIColor colorWithRed:70/255.0 green:130/255.0 blue:180/255.0 alpha:1.0]
+// 淡蓝色
+#define LIGHT_SKY_BLUE    [UIColor colorWithRed:135/255.0 green:206/255.0 blue:250/255.0 alpha:1.0]
+// 天蓝色
+#define SKY_BLUE          [UIColor colorWithRed:135/255.0 green:206/255.0 blue:235/255.0 alpha:1.0]
+// 深天蓝
+#define DEEP_SKY_BLUE     [UIColor colorWithRed:240/255.0 green:248/255.0 blue:255/255.0 alpha:1.0]
+// 淡蓝
+#define LIGHT_BLUE        [UIColor colorWithRed:173/255.0 green:216/255.0 blue:230/255.0 alpha:1.0]
+// 火药蓝
+#define POWDER_BLUE       [UIColor colorWithRed:176/255.0 green:224/255.0 blue:230/255.0 alpha:1.0]
+// 军校蓝
+#define CADET_BLUE        [UIColor colorWithRed:95/255.0 green:158/255.0 blue:160/255.0 alpha:1.0]
+// 蔚蓝色
+#define AZURE             [UIColor colorWithRed:240/255.0 green:255/255.0 blue:255/255.0 alpha:1.0]
+// 淡青色
+#define LIGHT_CYAN        [UIColor colorWithRed:225/255.0 green:248/255.0 blue:255/255.0 alpha:1.0]
+// 苍白的绿宝石
+#define PALETURQUOISE     [UIColor colorWithRed:175/255.0 green:238/255.0 blue:238/255.0 alpha:1.0]
+// 青色
+#define CYAN              [UIColor colorWithRed:0/255.0 green:255/255.0 blue:255/255.0 alpha:1.0]
+// 水绿色
+#define AQUA              [UIColor colorWithRed:0/255.0 green:255/255.0 blue:255/255.0 alpha:1.0]
+// 深绿宝石
+#define DARK_TURQUOISE    [UIColor colorWithRed:0/255.0 green:206/255.0 blue:209/255.0 alpha:1.0]
+// 深石板灰
+#define DARK_SLATE_GRAY   [UIColor colorWithRed:49/255.0 green:79/255.0 blue:79/255.0 alpha:1.0]
+// 深青色
+#define DARK_CYAN         [UIColor colorWithRed:0/255.0 green:139/255.0 blue:139/255.0 alpha:1.0]
+// 水鸭色
+#define TEAL              [UIColor colorWithRed:0/255.0 green:128/255.0 blue:128/255.0 alpha:1.0]
+// 适中的绿宝石
+#define MEDIUM_TURQUOISE  [UIColor colorWithRed:72/255.0 green:209/255.0 blue:204/255.0 alpha:1.0]
+// 浅海洋绿
+#define LIGHT_SEA_GREEN   [UIColor colorWithRed:32/255.0 green:178/255.0 blue:170/255.0 alpha:1.0]
+// 绿宝石
+#define TURQUOISE         [UIColor colorWithRed:64/255.0 green:224/255.0 blue:208/255.0 alpha:1.0]
+// 绿玉
+#define BABY_GREEN        [UIColor colorWithRed:127/255.0 green:255/255.0 blue:170/255.0 alpha:1.0]
+// 适中的碧绿色
+#define MEDIUM_AQUAMARINE [UIColor colorWithRed:0/255.0 green:250/255.0 blue:154/255.0 alpha:1.0]
+// 适中的春天的绿色
+#define MEDIUM_SPRING_GREEN [UIColor colorWithRed:245/255.0 green:255/255.0 blue:250/255.0 alpha:1.0]
+// 薄荷奶油
+#define MINT_CREAM        [UIColor colorWithRed:0/255.0 green:255/255.0 blue:127/255.0 alpha:1.0]
+// 春天的绿色
+#define SPRING_GREEN      [UIColor colorWithRed:60/255.0 green:179/255.0 blue:113/255.0 alpha:1.0]
+// 海洋绿
+#define SEA_GREEN         [UIColor colorWithRed:46/255.0 green:139/255.0 blue:87/255.0 alpha:1.0]
+// 蜂蜜
+#define HONEY_DEW         [UIColor colorWithRed:240/255.0 green:255/255.0 blue:0/255.0 alpha:1.0]
+// 淡绿色
+#define LIGHT_GREEN       [UIColor colorWithRed:144/255.0 green:238/255.0 blue:144/255.0 alpha:1.0]
+// 苍白的绿色
+#define PALE_GREEN        [UIColor colorWithRed:152/255.0 green:251/255.0 blue:252/255.0 alpha:1.0]
+// 深海洋绿
+#define DARK_SEA_GREEN    [UIColor colorWithRed:143/255.0 green:188/255.0 blue:143/255.0 alpha:1.0]
+// 酸橙绿
+#define LIME_GREEN        [UIColor colorWithRed:50/255.0 green:205/255.0 blue:50/255.0 alpha:1.0]
+// 酸橙色
+#define LIME              [UIColor colorWithRed:0/255.0 green:255/255.0 blue:0/255.0 alpha:1.0]
+// 森林绿
+#define FOREST_GREEN      [UIColor colorWithRed:34/255.0 green:139/255.0 blue:34/255.0 alpha:1.0]
+// 纯绿
+#define GREEN             [UIColor colorWithRed:0/255.0 green:255/255.0 blue:0/255.0 alpha:1.0]
+// 深绿色
+#define DARK_GREEN        [UIColor colorWithRed:0/255.0 green:100/255.0 blue:0/255.0 alpha:1.0]
+// 查特酒绿
+#define CHART_REUSE       [UIColor colorWithRed:127/255.0 green:255/255.0 blue:0/255.0 alpha:1.0]
+// 草坪绿
+#define LAWN_GREEN        [UIColor colorWithRed:124/255.0 green:252/255.0 blue:0/255.0 alpha:1.0]
+// 绿黄色
+#define GREEN_YELLOW      [UIColor colorWithRed:173/255.0 green:255/255.0 blue:47/255.0 alpha:1.0]
+// 橄榄土褐色
+#define OLIVE_DRAB        [UIColor colorWithRed:75/255.0 green:107/255.0 blue:47/255.0 alpha:1.0]
+// 米色
+#define BEIGE             [UIColor colorWithRed:107/255.0 green:142/255.0 blue:35/255.0 alpha:1.0]
+// 浅秋麒麟黄
+#define LIGHT_GOLDENROD_YELLOW [UIColor colorWithRed:250/255.0 green:250/255.0 blue:210/255.0 alpha:1.0]
+// 象牙色
+#define IVORY             [UIColor colorWithRed:255/255.0 green:255/255.0 blue:245/255.0 alpha:1.0]
+// 浅黄色
+#define LIGHT_YELLOW      [UIColor colorWithRed:255/255.0 green:255/255.0 blue:224/255.0 alpha:1.0]
+// 黄色
+#define YELLOW            [UIColor colorWithRed:255/255.0 green:255/255.0 blue:0/255.0 alpha:1.0]
+// 橄榄
+#define OLIVE             [UIColor colorWithRed:128/255.0 green:128/255.0 blue:128/255.0 alpha:1.0]
+// 深卡其布
+#define DARK_KHAKI        [UIColor colorWithRed:189/255.0 green:183/255.0 blue:107/255.0 alpha:1.0]
+// 柠萌薄纱
+#define LEMON_CHIFFON     [UIColor colorWithRed:255/255.0 green:250/255.0 blue:205/255.0 alpha:1.0]
+// 秋灰麒麟
+#define PALE_GODENROD     [UIColor colorWithRed:238/255.0 green:232/255.0 blue:170/255.0 alpha:1.0]
+// 卡其布
+#define KHAKI             [UIColor colorWithRed:240/255.0 green:230/255.0 blue:140/255.0 alpha:1.0]
+// 金
+#define GOLD              [UIColor colorWithRed:255/255.0 green:215/255.0 blue:0/255.0 alpha:1.0]
+// 玉米色
+#define CORNISLK          [UIColor colorWithRed:255/255.0 green:248/255.0 blue:220/255.0 alpha:1.0]
+// 秋麒麟
+#define GOLD_ENROD        [UIColor colorWithRed:218/255.0 green:165/255.0 blue:32/255.0 alpha:1.0]
+// 花的白色
+#define FLORAL_WHITE      [UIColor colorWithRed:255/255.0 green:250/255.0 blue:240/255.0 alpha:1.0]
+// 老饰带
+#define OLD_LACE          [UIColor colorWithRed:253/255.0 green:245/255.0 blue:230/255.0 alpha:1.0]
+// 小麦色
+#define WHEAT             [UIColor colorWithRed:245/255.0 green:222/255.0 blue:179/255.0 alpha:1.0]
+// 鹿皮鞋
+#define MOCCASIN          [UIColor colorWithRed:255/255.0 green:228/255.0 blue:181/255.0 alpha:1.0]
+// 橙色
+#define ORANGE            [UIColor colorWithRed:255/255.0 green:165/255.0 blue:0/255.0 alpha:1.0]
+// 番木瓜
+#define PAPAYA_WHIP       [UIColor colorWithRed:255/255.0 green:239/255.0 blue:213/255.0 alpha:1.0]
+// 漂白的杏仁
+#define BLANCHED_ALMOND   [UIColor colorWithRed:255/255.0 green:235/255.0 blue:205/255.0 alpha:1.0]
+// Navajo白
+#define NAVAJO_WHITE      [UIColor colorWithRed:255/255.0 green:222/255.0 blue:173/255.0 alpha:1.0]
+// 古代的白色
+#define ANTIQUE_WHITE     [UIColor colorWithRed:250/255.0 green:235/255.0 blue:215/255.0 alpha:1.0]
+// 晒黑
+#define TAN               [UIColor colorWithRed:210/255.0 green:180/255.0 blue:140/255.0 alpha:1.0]
+// 结实的树
+#define BRULY_WOOD        [UIColor colorWithRed:222/255.0 green:184/255.0 blue:135/255.0 alpha:1.0]
+// 乳脂 番茄
+#define BISQUE            [UIColor colorWithRed:255/255.0 green:228/255.0 blue:196/255.0 alpha:1.0]
+// 深橙色
+#define DARK_ORANGE       [UIColor colorWithRed:255/255.0 green:140/255.0 blue:0/255.0 alpha:1.0]
+// 亚麻布
+#define LINEN             [UIColor colorWithRed:250/255.0 green:240/255.0 blue:230/255.0 alpha:1.0]
+// 秘鲁
+#define PERU              [UIColor colorWithRed:205/255.0 green:133/255.0 blue:63/255.0 alpha:1.0]
+// 桃色
+#define PEACHPUFF         [UIColor colorWithRed:255/255.0 green:218/255.0 blue:185/255.0 alpha:1.0]
+// 沙棕色
+#define SANDY_BROWN       [UIColor colorWithRed:244/255.0 green:164/255.0 blue:96/255.0 alpha:1.0]
+// 巧克力
+#define CHOCOLATE         [UIColor colorWithRed:210/255.0 green:105/255.0 blue:30/255.0 alpha:1.0]
+// 马鞍棕色
+#define SADDLE_BROWN      [UIColor colorWithRed:139/255.0 green:69/255.0 blue:19/255.0 alpha:1.0]
+// 海贝壳
+#define SEA_SHELL         [UIColor colorWithRed:255/255.0 green:245/255.0 blue:238/255.0 alpha:1.0]
+// 黄土赭色
+#define SIENNA            [UIColor colorWithRed:160/255.0 green:82/255.0 blue:45/255.0 alpha:1.0]
+// 浅鲜肉色
+#define LIGHTSALMON       [UIColor colorWithRed:255/255.0 green:160/255.0 blue:122/255.0 alpha:1.0]
+// 珊瑚
+#define CORAL             [UIColor colorWithRed:255/255.0 green:127/255.0 blue:80/255.0 alpha:1.0]
+// 橙红色
+#define ORANGE_RED        [UIColor colorWithRed:255/255.0 green:69/255.0 blue:0/255.0 alpha:1.0]
+// 深鲜肉色
+#define DARK_SALMON       [UIColor colorWithRed:233/255.0 green:150/255.0 blue:122/255.0 alpha:1.0]
+// 番茄
+#define TOMATO            [UIColor colorWithRed:255/255.0 green:99/255.0 blue:71/255.0 alpha:1.0]
+// 薄雾玫瑰
+#define MISTY_ROSE        [UIColor colorWithRed:255/255.0 green:228/255.0 blue:225/255.0 alpha:1.0]
+// 鲜肉色
+#define SALMON            [UIColor colorWithRed:250/255.0 green:128/255.0 blue:114/255.0 alpha:1.0]
+// 雪
+#define SNOW              [UIColor colorWithRed:255/255.0 green:250/255.0 blue:250/255.0 alpha:1.0]
+// 淡珊瑚色
+#define LIGHT_CORAL       [UIColor colorWithRed:240/255.0 green:128/255.0 blue:128/255.0 alpha:1.0]
+// 玫瑰棕色
+#define RISY_BROWN        [UIColor colorWithRed:188/255.0 green:143/255.0 blue:143/255.0 alpha:1.0]
+// 印度红
+#define INDIA_RED         [UIColor colorWithRed:205/255.0 green:92/255.0 blue:92/255.0 alpha:1.0]
+// 纯红
+#define RED               [UIColor colorWithRed:255/255.0 green:0/255.0 blue:0/255.0 alpha:1.0]
+// 棕色
+#define BROWN             [UIColor colorWithRed:165/255.0 green:42/255.0 blue:42/255.0 alpha:1.0]
+// 耐火砖
+#define FIRE_BRICK        [UIColor colorWithRed:178/255.0 green:34/255.0 blue:34/255.0 alpha:1.0]
+// 深红色
+#define DARK_RED          [UIColor colorWithRed:139/255.0 green:0/255.0 blue:0/255.0 alpha:1.0]
+// 栗色
+#define MAROON            [UIColor colorWithRed:128/255.0 green:0/255.0 blue:0/255.0 alpha:1.0]
+// 纯白
+#define WHITE             [UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:1.0]
+// 白烟
+#define WHITE_SMOKE       [UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1.0]
+// Gainsboro
+#define GAINSBORO         [UIColor colorWithRed:220/255.0 green:220/255.0 blue:220/255.0 alpha:1.0]
+// 浅灰色
+#define LIGHT_GRAY        [UIColor colorWithRed:211/255.0 green:211/255.0 blue:211/255.0 alpha:1.0]
+// 银白色
+#define SLIVER            [UIColor colorWithRed:192/255.0 green:192/255.0 blue:192/255.0 alpha:1.0]
+// 深灰色
+#define DARK_GRAY         [UIColor colorWithRed:169/255.0 green:169/255.0 blue:169/255.0 alpha:1.0]
+// 灰色
+#define GRAY              [UIColor colorWithRed:128/255.0 green:128/255.0 blue:128/255.0 alpha:1.0]
+// 暗淡的灰色
+#define DIM_GRAY          [UIColor colorWithRed:105/255.0 green:105/255.0 blue:105/255.0 alpha:1.0]
+// 纯黑
+#define BLACK             [UIColor colorWithRed:0/255.0 green:0/255.0 blue:0/255.0 alpha:1.0]
+#endif /* CommonlyUsedColor_h */
